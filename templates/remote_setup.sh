b4918707f7c0f9f2f83607e6f99570959c2e55a2
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-04-12 23:33:11 +0200

    [workflow] Finished SYNC_TO_REMOTE and REMOTE_SETUP

--- templates/remote_setup.sh
+++ templates/remote_setup.sh
@@ -2,4 +2,22 @@
 
 set -eux -o pipefail
 
-echo "Hello!"
+cd %PLATFORMS.REMOTE.SCRATCH_DIR%
+
+# git clone only the directories for mHM test data
+# https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository
+# https://askubuntu.com/questions/460885/how-to-clone-only-some-directories-from-a-git-repository
+
+mkdir data
+cd data
+git init
+git remote add -f upstream https://github.com/mhm-ufz/mHM.git
+
+git config core.sparseCheckout true
+
+echo "test_domain/" >> .git/info/sparse-checkout
+echo "test_domain_2/" >> .git/info/sparse-checkout
+
+git pull upstream v5.12.0
+
+echo "REMOTE_SETUP complete!"
--- templates/sync_to_remote.sh
+++ templates/sync_to_remote.sh
@@ -2,4 +2,10 @@
 
 set -eux -o pipefail
 
-echo "Hello!"
+cd %PROJDIR%
+
+echo "Copying the mHM Singularity image mhm.sif via scp: %PLATFORMS.REMOTE.USER%@%PLATFORMS.REMOTE.HOST%:%PLATFORMS.REMOTE.SCRATCH_DIR%"
+
+scp mhm.sif %PLATFORMS.REMOTE.USER%@%PLATFORMS.REMOTE.HOST%:%PLATFORMS.REMOTE.SCRATCH_DIR%
+
+echo "SYNC_TO_REMOTE complete!"
