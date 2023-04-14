bfec4098992d46ff4e0e967d498fc6328062cbe5
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-04-14 18:41:40 +0200

    [workflow] Avoid re-creating the Docker and Singularity containers if the .sif file already exists in LOCAL_SETUP

--- templates/local_setup.sh
+++ templates/local_setup.sh
@@ -6,13 +6,17 @@ echo "Creating the Docker container for mHM v%MHM.VERSION%"
 
 cd %PROJDIR%
 
-docker build \
-  --no-cache=true \
-  --tag "auto-mhm-test-domains/mhm:v%MHM.VERSION%" .
+if [[ ! -f mhm.sif ]]; then
 
-echo "Creating the Singularity container for mHM v%MHM.VERSION%"
+  docker build \
+    --no-cache=true \
+    --tag "auto-mhm-test-domains/mhm:v%MHM.VERSION%" .
 
-sudo singularity build --force mhm.sif "docker-daemon://auto-mhm-test-domains/mhm:v%MHM.VERSION%"
+  echo "Creating the Singularity container for mHM v%MHM.VERSION%"
+
+  sudo singularity build --force mhm.sif "docker-daemon://auto-mhm-test-domains/mhm:v%MHM.VERSION%"
+
+fi
 
 singularity inspect --all mhm.sif
 
