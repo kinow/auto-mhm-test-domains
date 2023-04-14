5dcba0e446130946c6e4a29fcdec2923025a2234
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-04-14 23:12:48 +0200

    [workflow] Start work on the GRAPH task

---
+++ plot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Reads a given NetCDF file as input, and uses xarray and matplotlib
+# to create one plot of a variable for each time step available.
+# Source: https://stackoverflow.com/a/49043058
+
+from argparse import ArgumentParser
+
+from pathlib import Path
+
+import xarray as xr
+import matplotlib
+import matplotlib.pyplot as plt
+
+def main():
+    parser = ArgumentParser(description='Plot one PNG file to each time step value of a NetCDF variable')
+    parser.add_argument('-i', '--input', dest='input', required=True)
+    parser.add_argument('-v', '--variable', dest='variable', required=True)
+    parser.add_argument('-o', '--output', dest='output', required=True)
+
+    args = parser.parse_args()
+
+    matplotlib.use('Agg')
+    file_name = Path(args.input)
+    output_path = Path(args.output).resolve()
+    with xr.open_dataset(filename_or_obj=file_name, engine='netcdf4') as ds:
+        for t in range(ds.time.shape[0]):
+            da = ds[args.variable].isel(time=t)
+            plt.figure()
+            da.plot()
+            output_file = output_path / f'frame_{t}.png'
+            plt.savefig(str(output_file))
+
+
+if __name__ == '__main__':
+    main()
--- templates/graph.sh
+++ templates/graph.sh
@@ -2,4 +2,29 @@
 
 set -eux -o pipefail
 
-echo "Hello!"
+echo "Plotting PNG files for each timestep of the PET variable in the mHM_Fluxes_States.nc output file"
+
+function plot() {
+  OUTPUT_FOLDER=$1
+  echo "Using output from output folder: ${OUTPUT_FOLDER}"
+  singularity exec mhm.sif /opt/conda/bin/python plot.py \
+      --variable "PET" \
+      --input "${OUTPUT_FOLDER}/mHM_Fluxes_States.nc" \
+      --output "${OUTPUT_FOLDER}"
+  convert \
+      -delay 30 \
+      -loop 0 \
+      "${OUTPUT_FOLDER}/*.png" \
+      "${OUTPUT_FOLDER}/plot_${EVAL_PERIOD_START}_${EVAL_PERIOD_END}.gif"
+  OUTPUT_FOLDER_PARENT=$(basename "${OUTPUT_FOLDER}")
+  OUTPUT_FOLDER_BASENAME=$(dirname "${OUTPUT_FOLDER}")
+  tar \
+      -zcvf "mhm_output_${EVAL_PERIOD_START}_${EVAL_PERIOD_END}.tar.gz" \
+      -C "${OUTPUT_FOLDER_PARENT}" \
+      "${OUTPUT_FOLDER_BASENAME}"
+}
+
+plot "data/test_domain/output_b1/"
+plot "data/test_domain_2/output/"
+
+echo "GRAPH complete!"
