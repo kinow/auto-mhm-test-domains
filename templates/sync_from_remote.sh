a8e0a13ff76103118e76783730ffc8ddfbe6b468
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2023-04-16 14:33:01 +0200

    [workflow] Finished SYNC_FROM_REMOTE task

--- templates/sync_from_remote.sh
+++ templates/sync_from_remote.sh
@@ -2,4 +2,12 @@
 
 set -eux -o pipefail
 
-echo "Hello!"
+cd %PROJDIR%
+
+echo "Copying remote data"
+
+scp %PLATFORMS.REMOTE.USER%@%PLATFORMS.REMOTE.HOST%:%PLATFORMS.REMOTE.SCRATCH_DIR%/mhm_output*.tar.gz \
+    %PLATFORMS.REMOTE.USER%@%PLATFORMS.REMOTE.HOST%:%PLATFORMS.REMOTE.SCRATCH_DIR%/plot*.gif \
+    .
+
+echo "SYNC_FROM_REMOTE complete!"
